fa417acace0f1ea9e12c67dcb15836c43331f54c
Muhammad Khokhar <muhammad.t.k@hotmail.co.uk>
2018-10-07 20:10:04 +0000

    setup

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 CC=gcc
 CFLAGS=-lWarn -pedantic
 
+alarm: irtester.o libmyifttt.a
+	$(CC) irtester.o -L. -lwiringPi -lmyifttt -lcurl -o alarm
+
 tester: tester.o libmyifttt.a
 	$(CC) tester.o -L. -lmyifttt -lcurl -o tester
 
@@ -13,5 +16,10 @@ ifttt.o: 	ifttt.c ifttt.h
 tester.o:	tester.c ifttt.h
 	$(CC) $(CFLAGS) -c -ansi $<
 
+irtester.o:	irtester.c ifttt.h
+	$(CC) $(CFLAGS) -c -ansi $<
+
 clean:
 	rm tester *.o
+
+all: tester alarm
--- irtester.c
+++ irtester.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
 #include <wiringPi.h>
+#include "ifttt.h"
+#include <curl/curl.h>
 
 int main(int argc, char *argv[])
 {
   int i;
   wiringPiSetup () ;
-  pinMode(0, INPUT);
+  pinMode(0, INPUT); /* PIR SENSOR */
+  pinMode(1, OUTPUT); /* GREEN LED */
+  pinMode(2, OUTPUT); /* RED LED */
   while(1) {
     printf("Waiting for reset\n");
-    while(digitalRead(0) == 1);
+    while(digitalRead(0) == 1)
+    {
+	if(digitalRead(0) == 1)
+	{
+		ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "Recieved", "Status Update", "Alarm Triggered");
+		digitalWrite(2, HIGH);
+		delay(500);
+		digitalWrite(2, LOW);
+		delay(500);
+	}
+    }
     printf("Waiting for event\n");
-    while(digitalRead(0) == 0);
+    while(digitalRead(0) == 0)
+    {   /*
+	if(digitalRead(0) == 1)
+	{
+		digitalWrite(1, HIGH);
+		delay(500);
+		digitalWrite(1, LOW);
+		delay(500);
+	}
+	*/
+	if(digitalRead(0) == 0)
+	{
+		digitalWrite(1, HIGH);
+		delay(500);
+		digitalWrite(1, LOW);
+		delay(500);
+	}
+    }
     printf("Alarm\n");
   }
   /*NOTREACHED*/
